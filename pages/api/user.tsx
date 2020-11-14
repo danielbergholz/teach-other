c9b1ab4658ffc74560e4676d80bf4b5016f35e70
Daniel Bergholz <bergholz.daniel@gmail.com>
2020-11-14 13:38:21 -0300

    add time verification (cannot be between 20:00 and 7:00)

--- pages/api/user.tsx
+++ pages/api/user.tsx
@@ -1,6 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import connect from '../../utils/database';
 
+interface IAvailableHours {
+  monday: number[];
+  tuesday: number[];
+  wednesday: number[];
+  thursday: number[];
+  friday: number[];
+}
+
 interface ErrorResponseType {
   error: string;
 }
@@ -13,7 +21,7 @@ interface SuccessResponseType {
   teacher: true;
   coins: number;
   courses: string[];
-  available_hours: Record<string, number[]>;
+  available_hours: IAvailableHours;
   available_locations: string[];
   reviews: Record<string, unknown>[];
   appointments: Record<string, unknown>[];
@@ -40,8 +48,25 @@ export default async (
       teacher: boolean;
       courses: string[];
       available_locations: string[];
-      available_hours: Record<string, number[]>;
+      available_hours: IAvailableHours;
     } = req.body;
+
+    // check if available hours is between 7:00 and 20:00
+    let invalidHour = false;
+    for (const dayOfTheWeek in available_hours) {
+      available_hours[dayOfTheWeek].forEach((hour) => {
+        if (hour < 7 || hour > 20) {
+          invalidHour = true;
+          return;
+        }
+      });
+    }
+    if (invalidHour) {
+      res
+        .status(400)
+        .json({ error: 'You cannot teach between 20:00 and 7:00' });
+      return;
+    }
 
     if (!teacher) {
       if (!name || !email || !cellphone) {
